ca50545156ae587b1b499d99ae046f0447abbf55
kevinmou733 <2398094726@qq.com>
2026-04-05 01:49:33 +0800

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,29 @@
+import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
+val localProperties = Properties()
+val localFile = rootProject.file("local.properties")
+if (localFile.exists()) {
+    localFile.inputStream().use { stream ->
+        localProperties.load(stream)
+    }
+}
+
+// 读取两个Key
+val AMAP_WEB_KEY = localProperties.getProperty("AMAP_WEB_KEY", "")
+val AMAP_ANDROID_KEY = localProperties.getProperty("AMAP_ANDROID_KEY", "")
+
 android {
     namespace = "com.example.netgeocourier"
     compileSdk = 35
+
+    buildFeatures {
+        buildConfig = true
+    }
 
     defaultConfig {
         applicationId = "com.example.netgeocourier"
@@ -16,8 +33,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        buildConfigField("String", "AMAP_WEB_KEY", "\"${project.findProperty("AMAP_WEB_KEY") ?: ""}\"")
+        buildConfigField("String", "AMAP_WEB_KEY", "\"$AMAP_WEB_KEY\"")
+        manifestPlaceholders["AMAP_KEY"] = AMAP_ANDROID_KEY
+        //buildConfigField("String", "AMAP_WEB_KEY", "\"${project.findProperty("AMAP_WEB_KEY") ?: ""}\"")
+        //manifestPlaceholders["AMAP_KEY"] = project.findProperty("AMAP_ANDROID_KEY") ?: ""
     }
 
     buildTypes {
@@ -27,6 +46,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
@@ -52,6 +72,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -59,4 +80,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
 }
