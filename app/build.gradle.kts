7360ee88068fbbe5febf2cb845b759752b231acd
AK47349 <469037621@qq.com>
2026-04-08 20:54:37 +0800

    加上了开机动画

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 
 }
